0877756fcee7d79adb95303e1d10e6637247e904
tatsiana1707 <tannysha1987@mail.ru>
2022-11-05 13:43:12 +0300

    fix CONST REDIRECT_URL

--- src/api/const.js
+++ src/api/const.js
@@ -3,5 +3,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'JRdMKDbFh45VvDX-wn-Veg';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://blogget-beta.vercel.app/auth';
 export const SCOPE = 'identity submit read';
